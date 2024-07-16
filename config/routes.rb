67b64e1c18efb061dccacc637eae6470a2601c72
EC2 Default User <padosyu1127@gmail.com>
2024-07-16 11:40:19 +0000

    Top make

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
     devise_for :users
  resources :books,only:[:new,:show,:edit,:index,:create,:destroy]
- resources :users,only:[:show,:edit,:update]
+ resources :users,only:[:index,:show,:edit,:update]
+ get "/about" => "homes#about"
  
   root to:"homes#top"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html

5c9834851d7f0684c7b0e1ee38147a3e7f2a7c5c
EC2 Default User <padosyu1127@gmail.com>
2024-07-25 11:08:10 +0000

    user_show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,9 +8,25 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
+    @book_new=Book.new
+    @books=@user.books
   end
 
   def edit
     @user = User.find(params[:id])
   end
+  
+  def update
+    @user=User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path
+  end
+  
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:name,:introduction)
+  end
+  
 end
